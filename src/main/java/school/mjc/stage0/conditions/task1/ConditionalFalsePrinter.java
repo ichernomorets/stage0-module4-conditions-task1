49291abe6b12f4bbe548c5e4f5e0d1c08b3c272d
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-11-28 20:09:18 +0100

    Tasks completed

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean isTrue = false;
+        if(isTrue == true){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
     }
 }
